30a6e79dc7a6e67063e3f8dfbc8ccb755940395b
AntonioDattolo <antonio19794.a@gmail.com>
2024-05-02 21:26:42 +0200

    MILESTONE 5

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ let grid = document.getElementById("my-grid");
 let play = document.getElementById("play");
 let diff;
 let punt = document.getElementById("punteggio");
-let score = 0;
+let score = 0; 
 let contBomb =[];
 
 //FUNZIONE PER DAR INIZIO AL CICLO DELLA CREAZIONE DELLA GRIGLIA
@@ -13,48 +13,35 @@ function start(){
     grid.innerHTML ="";
     diff = document.getElementById("difficult").value;
     contBomb =[];
-    generateBomb() 
+    generateBomb()
+    score = 0;
+    punt.innerHTML= "";
+    punt.classList.remove("winner")
+    
 
     if( diff == "easy"){
         for (let x = 1; x <= 100; x++) {
             let gridCreations = createSquare(x);
             grid.append(gridCreations) ;                    
-            // if(contBomb.includes(x)){
-            //      grid.append(gridCreations)
-            //      gridCreations.classList.add("bomb")
-            // }
-            
         }
     } else if( diff == "medium"){ 
         for (let x = 1; x <= 81; x++) {
             let gridCreations = createSquare(x); 
             grid.append(gridCreations);
-            // if(contBomb.includes(x)){
-            //     grid.append(gridCreations)
-            //     gridCreations.classList.add("bomb")
-            // }
-           
         }       
     } else if( diff == "hard"){
         for (let x = 1; x <= 49; x++) {
             let gridCreations = createSquare(x); 
-            grid.append(gridCreations);
-            // if(contBomb.includes(x)){
-            //     grid.append(gridCreations)
-            //     gridCreations.classList.add("bomb")
-            // } 
-         
-           
+            grid.append(gridCreations); 
         }  
     }
-}
+   
+} 
 // ***** GLI "IF" COMMENTATI SONO UN ALTERNATIVA
 
 // ***************FUNZIONE PER LA CREAZIONE DEI QUADRATI DELLA GRIGLIA
 function createSquare(y){
-    // let diff = document.getElementById("difficult").value;
     let square = document.createElement("div");
-    // grid.append(square)
     if (diff == "easy"){
         square.classList.add("base-square");
         square.classList.add("bg-square")
@@ -68,37 +55,51 @@ function createSquare(y){
         square.classList.add("bg-square")
     }        
 
-    //  square.innerHTML = y;
+    
     square.addEventListener("click", function star(){
+        
         if(contBomb.includes(y)){
             let lose = "YOU LOSE" ;
             square.innerHTML += ` ${y} è una bomba,YOU LOSE`;
             square.classList.toggle("bomb");
-            square.classList.remove("bg-square")
+            
             punt.innerHTML = `YOU LOSE, ur SCORE IS ${score}`;
-            grid.classList.add("stop")
-                      
-                
-                
-        }else if(contBomb.includes(y)== false){
+            grid.classList.add("stop")   
+        }else {
             square.innerHTML += `il numero della cella è ${y}`;
             square.classList.toggle("selected");
             score++;
-            punt.innerHTML = score;
-                
+            punt.innerHTML = score;    
         }
-        
+        //CICLO WHILE PER CONTROLLO VINCITORE
+        x=0
+        while(x < 100){
+            if(score==2 && diff == "hard" ){
+                punt.innerHTML = `YOU WIN! UR SCORE IS ${score}`;
+                punt.classList.add("winner");
+                grid.classList.add("stop")
+                
+
+            }else if(score==65 && diff== "medium"){
+                punt.innerHTML = `YOU WIN! UR SCORE IS ${score}`;
+                punt.classList.add("winner");
+                grid.classList.add("stop")
+                
+                
+            }else if(score == 84 && diff =="hard"){
+                punt.innerHTML = `YOU WIN! UR SCORE IS ${score}`;
+                punt.classList.add("winner");
+                grid.classList.add("stop")
+                
+            }
+            x++
+        }
     },{once:true})
-   
-    return square   
+      
+    return square 
 }
-
-
 // EVENTO CHE DA IL VIA ALLA CREAZIONE DELLA GRIGLIA
 play.addEventListener("click", start)
-
-
- 
 
 /// generazione bombe
 function generateBomb(){
